798b300915cf4673c32dace603c73f2e622b511b
KingCatto <sallad2012@gmail.com>
2025-01-23 10:18:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -312,7 +312,7 @@ function renderGames(append = false) {
                 <div class="game-title">${game.parsedTitle}</div>
                 ${game.version ? `<div class="game-meta">Version: <span>${game.version}</span></div>` : ''}
                 <div class="game-meta">Branch: <span>${game.branch || 'N/A'}</span></div>
-                <div class="game-meta">Updated: <span>${new Date(game.date).toLocaleString()}</span></div>
+                <div class="game-meta">Updated: <span>${game.date}</span></div>
                 <div class="game-platforms">
                     ${game.platforms.map(platform => 
                         `<div class="platform-tag">${platform}</div>`
@@ -352,7 +352,7 @@ function renderRepacks(append = false) {
                 <div class="game-title">${repack.parsedTitle}</div>
                 ${repack.version ? `<div class="game-meta">Version: <span>${repack.version}</span></div>` : ''}
                 <div class="game-meta">Branch: <span>${repack.branch || 'N/A'}</span></div>
-                <div class="game-meta">Updated: <span>${new Date(repack.date).toLocaleString()}</span></div>
+                <div class="game-meta">Updated: <span>${repack.date}</span></div>
                 <div class="game-links">
                     <a href="${repack.forum}" class="forum-link" target="_blank">Forum Page</a>
                     ${repack.store ? `<a href="${repack.store}" class="store-link" target="_blank">Steam Store</a>` : ''}
